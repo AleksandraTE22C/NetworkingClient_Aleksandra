f5910b0abb8e520ccc862a1786dff3aaf5371f4c
AleksandraTE22C <aleksandra.kajak-tonkiel@elev.ga.ntig.se>
2025-05-26 18:08:28 +0200

    Klar med koden

--- src/NetworkingClient.java
+++ src/NetworkingClient.java
@@ -6,8 +6,8 @@ public class NetworkingClient {
         Socket client = null;
 
         //Default port number we are going to use
-        int portnumber = 4747;
-        if(args.lenght >= 1){
+        int portnumber = 2525;
+        if(args.length >= 1){
             portnumber = Integer.parseInt(args[0]);
         }
 
@@ -36,6 +36,20 @@ public class NetworkingClient {
                 //to the output stream of the client socket.
                 msg = stdIn.readLine().trim();
                 pw.println(msg);
+
+                //Read data from the input stream of the client socket.
+                System.out.println("Message returned from the server = " + br.readLine());
+
+                pw.close();
+                br.close();
+                client.close();
+
+                //Stop the operation
+                if(msg.equalsIgnoreCase("Bye")){
+                    break;
+                }
+            } catch (IOException ie){
+                System.out.println("I/O error " + ie);
             }
         }
     }
